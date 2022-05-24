1d450a22022fb9e37eb6394fcec8a9c08e16ae0e
totalhack <none@none.com>
2022-05-24 16:14:53 -0400

    checkpoint: build and deploy setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+const path = require('path');
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.js'),
+      name: 'FormKitMultiStep',
+      fileName: (format) => `formkit-multistep.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
   plugins: [vue()]
 })
