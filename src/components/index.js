4b367fd432a76d94469ae4621400e01cf7fddd04
totalhack <none@none.com>
2023-05-02 10:39:47 -0400

    feat: export keyValOverlap

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import { default as FormKitMultiStep } from './FormKitMultiStep.vue';
 import usePrepop from '../usePrepop.js'
 import useSteps from '../useSteps.js'
-import { postData, getRedirect, redirectTo, openNewTab, strSubUrl, sleep } from '../utils.js'
+import { postData, getRedirect, redirectTo, openNewTab, strSubUrl, sleep, keyValOverlap } from '../utils.js'
 
-export { FormKitMultiStep, usePrepop, useSteps, postData, getRedirect, redirectTo, openNewTab, strSubUrl, sleep }
+export { FormKitMultiStep, usePrepop, useSteps, postData, getRedirect, redirectTo, openNewTab, strSubUrl, sleep, keyValOverlap }
